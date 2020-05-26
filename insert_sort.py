859ae237580cde2eb887d022e26f7f92cb7593bb
= <migonzalez1991@gmail.com>
2020-05-26 21:45:46 +0200

    Follow Python conventions

--- insert_sort.py
+++ insert_sort.py
@@ -8,80 +8,86 @@ from manimlib.scene.scene import Scene
 
 class InsertSort(Scene):
     nodes = []
+
     def construct(self):
-        self.nodes = [self.buildTextAndCircles(6, 3*LEFT), self.buildTextAndCircles(2, 2*LEFT), self.buildTextAndCircles(7, LEFT),
-                      self.buildTextAndCircles(1, 0), self.buildTextAndCircles(8, RIGHT), self.buildTextAndCircles(9, 2*RIGHT), self.buildTextAndCircles(4, 3*RIGHT)
+        self.nodes = [self.build_text_and_circles(6, 3 * LEFT), self.build_text_and_circles(2, 2 * LEFT),
+                      self.build_text_and_circles(7, LEFT), self.build_text_and_circles(1, 0),
+                      self.build_text_and_circles(8, RIGHT), self.build_text_and_circles(9, 2 * RIGHT),
+                      self.build_text_and_circles(4, 3 * RIGHT)
                       ]
-        self.initAnimation()
+        self.init_animation()
         self.wait()
         self.sort()
         self.finished()
         self.wait(2)
 
     def finished(self):
-        nbIterationsObj = TextMobject("Sorted")
-        nbIterationsObj.move_to(2*UP)
-        self.add(nbIterationsObj)
+        sorted_text = TextMobject("Sorted")
+        sorted_text.move_to(2*UP)
+        self.add(sorted_text)
 
         for node in self.nodes:
             node.circle.set_color(GREEN)
 
     def sort(self):
-        nodesLength = len(self.nodes)
-        arrow = Arrow(UP+RIGHT) #I don't get it
+        nodes_length = len(self.nodes)
+        arrow = Arrow(UP+RIGHT)  # I don't get it
         arrow.scale(1)
         arrow.move_to(UP + 3 * LEFT)
         self.add(arrow)
-        for i in range (1, nodesLength):
+        for i in range(1, nodes_length):
             arrow.generate_target()
             arrow.target.move_to(self.nodes[i].circle.get_arc_center() + UP)
             self.play(MoveToTarget(arrow))
             j = i
             while j > 0 and self.nodes[j].value < self.nodes[j-1].value:
-                self.swapNodes(j, j - 1)
+                self.swap_nodes(j, j - 1)
                 j = j - 1
             self.wait(0.1)
 
-    def printNodes(self):
+    def print_nodes(self):
         for node in self.nodes:
             print(node.value)
 
-    def initAnimation(self):
-        circleAnimations = []
+    def init_animation(self):
+        circle_animations = []
         for node in self.nodes:
             self.add(node.text)
-            circleAnimations.append(GrowFromCenter(node.circle))
-        self.play(*circleAnimations)
+            circle_animations.append(GrowFromCenter(node.circle))
+        self.play(*circle_animations)
 
-    def swapNodes(self, fromIndex, toIndex):
-        fromNode = self.nodes[fromIndex]
-        toNode = self.nodes[toIndex]
+    def swap_nodes(self, from_index, to_index):
+        from_node = self.nodes[from_index]
+        to_node = self.nodes[to_index]
         
-        toCenter = toNode.circle.get_arc_center()
-        fromNode.text.generate_target()
-        fromNode.text.target.move_to(toCenter)
-        fromNode.circle.generate_target()
-        fromNode.circle.target.move_to(toCenter)
+        to_center = to_node.circle.get_arc_center()
+        from_node.text.generate_target()
+        from_node.text.target.move_to(to_center)
+        from_node.circle.generate_target()
+        from_node.circle.target.move_to(to_center)
 
-        fromCenter = fromNode.circle.get_arc_center()
-        toNode.text.generate_target()
-        toNode.text.target.move_to(fromCenter)
-        toNode.circle.generate_target()
-        toNode.circle.target.move_to(fromCenter)
-        self.play(MoveToTarget(fromNode.text), MoveToTarget(fromNode.circle), MoveToTarget(toNode.text), MoveToTarget(toNode.circle))
+        from_center = from_node.circle.get_arc_center()
+        to_node.text.generate_target()
+        to_node.text.target.move_to(from_center)
+        to_node.circle.generate_target()
+        to_node.circle.target.move_to(from_center)
+        self.play(MoveToTarget(from_node.text), MoveToTarget(from_node.circle),
+                  MoveToTarget(to_node.text), MoveToTarget(to_node.circle))
 
-        self.nodes[fromIndex] = toNode
-        self.nodes[toIndex] = fromNode
+        self.nodes[from_index] = to_node
+        self.nodes[to_index] = from_node
 
-    def buildTextAndCircles(self, value, location):
-        textObj = TextMobject(str(value))
-        textObj.move_to(location)
-        circleObj = Circle()
-        circleObj.surround(textObj)
-        circleObj.scale(1.5)
-        return Node(textObj, circleObj, value)
+    @staticmethod
+    def build_text_and_circles(value, location):
+        text_obj = TextMobject(str(value))
+        text_obj.move_to(location)
+        circle_obj = Circle()
+        circle_obj.surround(text_obj)
+        circle_obj.scale(1.5)
+        return Node(text_obj, circle_obj, value)
 
-class Node():
+
+class Node:
     def __init__(self, text, circle, value):
         self.text = text
         self.circle = circle
--- select_sort.py
+++ select_sort.py
@@ -5,99 +5,105 @@ from manimlib.mobject.geometry import Arrow, Circle
 from manimlib.mobject.svg.tex_mobject import TextMobject
 from manimlib.scene.scene import Scene
 
+
 class SelectSort(Scene):
     nodes = []
+
     def construct(self):
-        self.nodes = [self.buildTextAndCircles(6, 3*LEFT), self.buildTextAndCircles(2, 2*LEFT), self.buildTextAndCircles(7, LEFT),
-                      self.buildTextAndCircles(1, 0), self.buildTextAndCircles(8, RIGHT), self.buildTextAndCircles(9, 2*RIGHT), self.buildTextAndCircles(4, 3*RIGHT)
+        self.nodes = [self.build_text_and_circles(6, 3 * LEFT), self.build_text_and_circles(2, 2 * LEFT),
+                      self.build_text_and_circles(7, LEFT), self.build_text_and_circles(1, 0),
+                      self.build_text_and_circles(8, RIGHT), self.build_text_and_circles(9, 2 * RIGHT),
+                      self.build_text_and_circles(4, 3 * RIGHT)
                       ]
-        self.initAnimation()
+        self.init_animation()
         self.wait()
         self.sort()
         self.finished()
         self.wait(2)
 
     def finished(self):
-        nbIterationsObj = TextMobject("Sorted")
-        nbIterationsObj.move_to(2*UP)
-        self.add(nbIterationsObj)
+        sorted_text = TextMobject("Sorted")
+        sorted_text.move_to(2*UP)
+        self.add(sorted_text)
 
     def sort(self):
-        nodesLength = len(self.nodes)
-        arrow = Arrow(UP+RIGHT) #I don't get it
+        nodes_length = len(self.nodes)
+        arrow = Arrow(UP+RIGHT)  # I don't get it
         arrow.scale(1)
         arrow.move_to(UP + 3 * LEFT)
         self.add(arrow)
-        for i in range (nodesLength):
+        for i in range(nodes_length):
             arrow.generate_target()
             arrow.target.move_to(self.nodes[i].circle.get_arc_center() + UP)
             self.play(MoveToTarget(arrow))
-            indexMin = self.findIndexMinimum(i)
-            if i != indexMin:
-                self.swapNodes(i, indexMin)
+            index_min = self.find_index_minimum(i)
+            if i != index_min:
+                self.swap_nodes(i, index_min)
             self.nodes[i].circle.set_color(GREEN)
 
-    def findIndexMinimum(self, fromIndex):
-        arrow = Arrow(DOWN+RIGHT) #I don't get it
+    def find_index_minimum(self, from_index):
+        arrow = Arrow(DOWN+RIGHT)  # I don't get it
         arrow.scale(0.5)
-        arrow.move_to(self.nodes[fromIndex].circle.get_arc_center() + DOWN)
+        arrow.move_to(self.nodes[from_index].circle.get_arc_center() + DOWN)
         arrow.set_color(BLUE)
         self.add(arrow)
-        indexMin = fromIndex
-        self.nodes[indexMin].circle.set_color(BLUE)
-        for i in range (fromIndex + 1, len(self.nodes)):
+        index_min = from_index
+        self.nodes[index_min].circle.set_color(BLUE)
+        for i in range(from_index + 1, len(self.nodes)):
             arrow.generate_target()
             arrow.target.move_to(self.nodes[i].circle.get_arc_center() + DOWN)
             self.play(MoveToTarget(arrow))
-            if self.nodes[i].value < self.nodes[indexMin].value:
-                self.nodes[indexMin].circle.set_color(RED)
-                indexMin = i
-                self.nodes[indexMin].circle.set_color(BLUE)
+            if self.nodes[i].value < self.nodes[index_min].value:
+                self.nodes[index_min].circle.set_color(RED)
+                index_min = i
+                self.nodes[index_min].circle.set_color(BLUE)
 
         self.remove(arrow)
-        return indexMin
+        return index_min
 
-
-    def printNodes(self):
+    def print_nodes(self):
         for node in self.nodes:
             print(node.value)
 
-    def initAnimation(self):
-        circleAnimations = []
+    def init_animation(self):
+        circle_animations = []
         for node in self.nodes:
             self.add(node.text)
-            circleAnimations.append(GrowFromCenter(node.circle))
-        self.play(*circleAnimations)
+            circle_animations.append(GrowFromCenter(node.circle))
+        self.play(*circle_animations)
 
-    def swapNodes(self, fromIndex, toIndex):
-        fromNode = self.nodes[fromIndex]
-        toNode = self.nodes[toIndex]
+    def swap_nodes(self, from_index, to_index):
+        from_node = self.nodes[from_index]
+        to_node = self.nodes[to_index]
         
-        toCenter = toNode.circle.get_arc_center()
-        fromNode.text.generate_target()
-        fromNode.text.target.move_to(toCenter)
-        fromNode.circle.generate_target()
-        fromNode.circle.target.move_to(toCenter)
+        to_center = to_node.circle.get_arc_center()
+        from_node.text.generate_target()
+        from_node.text.target.move_to(to_center)
+        from_node.circle.generate_target()
+        from_node.circle.target.move_to(to_center)
 
-        fromCenter = fromNode.circle.get_arc_center()
-        toNode.text.generate_target()
-        toNode.text.target.move_to(fromCenter)
-        toNode.circle.generate_target()
-        toNode.circle.target.move_to(fromCenter)
-        self.play(MoveToTarget(fromNode.text), MoveToTarget(fromNode.circle), MoveToTarget(toNode.text), MoveToTarget(toNode.circle))
+        from_center = from_node.circle.get_arc_center()
+        to_node.text.generate_target()
+        to_node.text.target.move_to(from_center)
+        to_node.circle.generate_target()
+        to_node.circle.target.move_to(from_center)
+        self.play(MoveToTarget(from_node.text), MoveToTarget(from_node.circle),
+                  MoveToTarget(to_node.text), MoveToTarget(to_node.circle))
 
-        self.nodes[fromIndex] = toNode
-        self.nodes[toIndex] = fromNode
+        self.nodes[from_index] = to_node
+        self.nodes[to_index] = from_node
 
-    def buildTextAndCircles(self, value, location):
-        textObj = TextMobject(str(value))
-        textObj.move_to(location)
-        circleObj = Circle()
-        circleObj.surround(textObj)
-        circleObj.scale(1.5)
-        return Node(textObj, circleObj, value)
+    @staticmethod
+    def build_text_and_circles(value, location):
+        text_obj = TextMobject(str(value))
+        text_obj.move_to(location)
+        circle_obj = Circle()
+        circle_obj.surround(text_obj)
+        circle_obj.scale(1.5)
+        return Node(text_obj, circle_obj, value)
 
-class Node():
+
+class Node:
     def __init__(self, text, circle, value):
         self.text = text
         self.circle = circle
